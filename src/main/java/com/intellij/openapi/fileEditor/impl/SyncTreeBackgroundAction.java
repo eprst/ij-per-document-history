520340c43cd48bf0954cfdd641a7cafc4d0be798
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2024-06-06 13:56:24 -0700

    misc updates

--- src/main/java/com/intellij/openapi/fileEditor/impl/SyncTreeBackgroundAction.java
+++ src/main/java/com/intellij/openapi/fileEditor/impl/SyncTreeBackgroundAction.java
@@ -1,14 +1,13 @@
 package com.intellij.openapi.fileEditor.impl;
 
+import com.intellij.ide.ui.LafManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.util.Pair;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.Enumeration;
 
 public class SyncTreeBackgroundAction extends AnAction {
   @Override
@@ -19,5 +18,7 @@ public class SyncTreeBackgroundAction extends AnAction {
     var defaults = UIManager.getDefaults();
     defaults.remove("Tree.background");
     defaults.put("Tree.background", editorBackgroundColor);
+
+    LafManager.getInstance().repaintUI();
   }
 }
